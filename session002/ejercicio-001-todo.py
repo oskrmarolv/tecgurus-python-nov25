744c51a1f324d5d9a9f7b3565cac49885541a3aa
Oscar Marquez <oscar.marq.olv@gmail.com>
2025-11-29 11:38:20 -0600

    [ `ejercicio` ] aplicacion To-Do
     Changes to be committed:
    	new file:   ejercicio-001-todo.py

---
+++ session002/ejercicio-001-todo.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+"""
+  Ejercicio: El Organizador de Tareas (Listas y Bucles while)
+  
+  Este ejercicio te ayudará a entender cómo manipular listas dinámicamente y cómo controlar el flujo del programa con un menú interactivo.
+
+  El Reto: Crea un programa que actúe como una lista de tareas "To-Do".
+
+  El programa debe iniciar con una lista vacía.
+
+  Debe mostrar un menú infinito (while True) con 3 opciones:
+
+  1: Agregar tarea.
+
+  2: Ver tareas pendientes.
+
+  3: Salir.
+
+  Si el usuario elige "1", pide el nombre de la tarea y agrégala a la lista.
+
+  Si elige "2", muestra todas las tareas usando un bucle for (imprime el número de tarea también).
+
+  Si elige "3", rompe el ciclo (break) y despídete.
+
+"""
