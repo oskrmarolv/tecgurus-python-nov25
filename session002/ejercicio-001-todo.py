38d9d7f96649d80d1a89cb192132802810e62185
Oscar Marquez <oscar.marq.olv@gmail.com>
2025-11-29 12:13:00 -0600

    [ `ejercicio` ] Ejercicio to-do resuelto

--- session002/ejercicio-001-todo.py
+++ session002/ejercicio-001-todo.py
@@ -24,3 +24,73 @@
   Si elige "3", rompe el ciclo (break) y despídete.
 
 """
+
+
+# -------------------------------------------
+# main --------------------------------------
+# -------------------------------------------
+
+task_list = [ "Subir material a la plataforma", "Subir ejemplos al repositorio", "Preparar presentacion" ]
+
+task_status = {
+  "success": 0,
+  "pending": 0, 
+  "skipped": 0
+}
+
+
+print("")
+print("+++ APLICACION DE TAREAS +++\n")
+print("")
+
+flag_control = True
+# while True:                         # requiere sentencia 'break'
+while flag_control:
+  task_status["pending"] = len(task_list)
+
+  print("")
+  print("Selecciona una opcion:")
+  print("\t1) Agregar nueva tarea")
+  print("\t2) Listar tareas")
+  print("\t3) Salir")
+  option = input(": ");
+
+  print("")
+
+  option = int(option);
+  match option:
+    case 1:
+      task = input("Nueva tarea: ")
+
+      task_list.append(task)
+
+    case 2:
+      print("Tareas pendientes")
+
+      for index,task in enumerate(task_list):
+        print(f"{index}) {task}")
+
+      print(task_status);
+
+    case 3:
+      """
+        Agregar un opcion donde permita indicar una tarea completada.
+        Se debe eliminar de la lista de tareas y actualizar el 
+        diccionario "task_status"
+      """
+      pass
+
+    case 4:
+      flag_control = False
+      break                         # si la definicion fue con "while True:"
+      pass
+
+    case _:
+      print("OPCION NO VALIDA! Por favor ingresa una opcion correcta.")
+
+
+# las estructuras de control no generan un ambito propio 
+# en la definicion de variables (usan el ambito del flujo 
+# principal)
+# print("scopes:", task, option)
+print("++ ADIOS!!")
