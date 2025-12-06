9c75d0bf0dde8a331d9cfc46b2974590948ec429
Oscar Marquez <oscar.marq.olv@gmail.com>
2025-12-06 11:09:53 -0600

    [ `cambio-menor` ] funcionalidad completada para opcion 2 (try-except-else-finally)

--- session003/ejercicio-001-controlGastos.py
+++ session003/ejercicio-001-controlGastos.py
@@ -30,8 +30,12 @@ monthly_expenses = {
 }
 unit_expenses = [ ]
 
+# currency = 0
+currency = 19.35
 
-print("CONTROL DE GASTOS +++");
+
+print("CONTROL DE GASTOS + + +");
+print("(MX) -> (US)");
 
 while True:
 	print("")
@@ -47,17 +51,74 @@ while True:
 		print(f"\t{'CATEGORIA':<15} | {'ACUMULADO':<15}")
 		print(f"\t{'-'*15} | {'-'*15}")
 		for category,amount in monthly_expenses.items():
-			print(f"\t{category:<15} | {amount:<15}")
+			print(f"\t{category:<15} | {amount} ({amount * currency} MX)")
 		print("")
 	
 	elif option == "2":
-		category = input("\tCategoria: ")
-		amount = input("tValor del gasto: ")
+		print("")
+		category = input("+ Categoria: ")
+		amount = input("+ Cantidad: ")
 
-		unit_expenses.append((category, amount))
+		# amount = float(amount)
 
-		print(unit_expenses)
-	
+		# MANEJO DE EXCEPCIONES EN PYTHON
+		# try - catch - finally 				otros lenguajes
+		# try - except - else - finally			en Python
+
+		try: 
+			# Bloque principal que buscar contener
+			# operaciones o flujos muy puntuales
+
+			# Operacion con posible error:
+			# 	el cast a un numero flotante no
+			# 	convierte cualquier string
+			amount = float(amount)
+
+			if amount == 0.0:
+				raise Exception("El monto no puede ser cero.", 100)
+			else:
+				amount = amount / currency
+
+		except ValueError as value:
+			print("")
+			print(f"Monto invalido: {value}!\nNo es posible registrar el monto para la categoria {category}.")
+			print("")
+
+		except ZeroDivisionError:
+			print("")
+			print(f"La conversion a moneda no es valida!\nNo es posible registrar el monto para la categoria {category}.")
+			print("")
+
+		except Exception as error:
+			# BaseException: error generico que captura casos no contemplados
+			# 	"as" crea una referencia a un objeto principal
+
+			print("")
+			# print(f"Operacion no valida!\n Causa: {error}")
+
+			# ERROR: __str__() se incoca y no tiene una representacion de ese tipo
+			# print(f"Operacion no valida!\n Causa: {error[0]} (codigo: {error[1]})")
+			print(f"Operacion no valida!\nCausa: {error.args[0]} (codigo x{error.args[1]})")
+			print("")
+
+		else:
+			# Bloque que solo se ejecuta si el bloque
+			# principal (try) no detona error alguno
+
+			# Se suele utilizar cuando se require 
+			# ejecutar operaciones secundarios
+
+			unit_expenses.append((category, amount))
+
+		finally:
+			# Bloque que siempre se ejecuta sin importar
+			# si existio error o no
+
+			# Se suele utilizar para asegurar que algunas 
+			# operaciones se ejecutaran 
+			print(f"+ Gastos unitarios en sesion:")
+			print(f"  {unit_expenses}" )
+
 	elif option == "3":
 		pass
 
