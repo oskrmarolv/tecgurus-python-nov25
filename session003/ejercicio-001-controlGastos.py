88a7addac82f8e5e4a20ba56af520b300c45d6e0
Oscar Marquez <oscar.marq.olv@gmail.com>
2025-12-06 13:34:57 -0600

    [ `cambio` ] funcionalidad completada para opcion 4 (files)

--- session003/ejercicio-001-controlGastos.py
+++ session003/ejercicio-001-controlGastos.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 """
-	MANEJO DE EXCEPCIONES
+	+ MANEJO DE EXCEPCIONES
 
 	ACCESO A ARCHIVOS
 
@@ -9,12 +9,56 @@
 """
 
 
+# INTRODUCCION AL MANEJO DE MODULOS
+# 
+# 	Agregando definiciones para su implementacion en el proyecto
+#		import NOMBRE_PAQUETE
+# 		from NOMBRE_PAQUETE import DEFINICION
+# 		from NOMBRE_PAQUETE import DEFINICION as REFERENCIA
 
-def update_monthly_expenses(expense_status, unit):
-	update_status = { }
+import math 			# definiciones de la biblioteca "math" de python-core
 
+import random			# definiciones de la biblioteca "random" de python-core
+
+from datetime import datetime
+
+# from datetime import datetime as timestamp
+
+
+
+def output_monthly_expenses(expense_status):
+	"""
+		Salida en consola de gastos
+
+		None output_monthly_expenses(dict expense_status)
+	"""
+	print("")
+	print(f"\t{'CATEGORIA':<15} | {'ACUMULADO':<15}")
+	print(f"\t{'-'*15} | {'-'*15}")
+	for category,amount in expense_status.items():
+		print(f"\t{category:<15} | {amount} ({amount * currency} MX)")
+	print("")	
+
+def update_monthly_expenses(unit, expense_status):
+	"""
+		Actualizacion de balance por categoria
+
+		tuple update_monthly_expenses(list unit, dict expense_status)
+	"""
+	# creando diccionario por comprension
+	update_status = { category:amount for category,amount in expense_status.items() }
+
+	# recorriendo montos unitarios
+	for category,amount in unit:
+		# ERROR: no puedo asegurar que "category" existe
+		# update_status[category] = update_status[category] + amount
+
+		if category not in update_status.keys():
+			update_status[category] = 0.0
+
+		update_status[category] += amount
+		
 	return update_status, [ ]
-	pass
 
 
 # -----------------------------------
@@ -22,22 +66,46 @@ def update_monthly_expenses(expense_status, unit):
 # -----------------------------------
 
 # SECCION DONDE RECUPERAMOS EL ULTIMO ESTADO DE "monthly_expenses"
-monthly_expenses = { 
-	"comida": 0.0,
-	"transporte": 0.0,
-	"recreacion": 0.0,
-	"suscripciones": 0.0
-}
+
+# MANEJO DE ARCHIVOS
+# 
+# Acceso a archivos
+#		* Creacion de archivos con notacion "snake_case"
+#		* La ruta del archivo se sugiere relativa
+#		
+# open(file, mode, encoding, newline)
+
+# Metodo modero (> 3.x.x)
+# 	use de with
+
+access_timestamp = datetime.now()
+
+
+monthly_expenses = None
+# monthly_expenses = { 
+# 	"comida": 0.0,
+# 	"transporte": 0.0,
+# 	"recreacion": 0.0,
+# 	"suscripciones": 0.0
+# }
+with open("balance_file.txt", 'r', encoding="utf-8") as last_balance_file:
+	data = last_balance_file.read()
+	monthly_expenses = eval(data)
+
 unit_expenses = [ ]
 
 # currency = 0
 currency = 19.35
 
-
 print("CONTROL DE GASTOS + + +");
 print("(MX) -> (US)");
+print("")
+print(f"Balance actual ({ access_timestamp.strftime("%Y/%m/%d %H:%M") }) =>")
+output_monthly_expenses(monthly_expenses)
 
-while True:
+
+flow_ctrl = True
+while flow_ctrl:
 	print("")
 	print("Selecciona una opcion:")
 	print("\t1) Balance actual de gastos")
@@ -47,12 +115,7 @@ while True:
 	option = input(": ");
 
 	if option == "1":
-		print("")
-		print(f"\t{'CATEGORIA':<15} | {'ACUMULADO':<15}")
-		print(f"\t{'-'*15} | {'-'*15}")
-		for category,amount in monthly_expenses.items():
-			print(f"\t{category:<15} | {amount} ({amount * currency} MX)")
-		print("")
+		output_monthly_expenses(monthly_expenses)
 	
 	elif option == "2":
 		print("")
@@ -62,8 +125,8 @@ while True:
 		# amount = float(amount)
 
 		# MANEJO DE EXCEPCIONES EN PYTHON
-		# try - catch - finally 				otros lenguajes
-		# try - except - else - finally			en Python
+		# 	try - catch - finally 					otros lenguajes
+		# 	try - except - else - finally			en Python
 
 		try: 
 			# Bloque principal que buscar contener
@@ -120,16 +183,36 @@ while True:
 			print(f"  {unit_expenses}" )
 
 	elif option == "3":
-		pass
+		monthly_expenses, unit_expenses = update_monthly_expenses(unit_expenses, monthly_expenses)
+
+		output_monthly_expenses(monthly_expenses)
 
 	elif option == "4":
+		# MANEJO DE ARCHIVOS
+		
+		# Metodo clasico (< 3.x.x)
+		# 	definicion de "fd" es completamente responsabilidad
+		#	del desarrollador (open(), close())
+	
+		try: 
+			# esto puede fallar y corromper el archivo si 
+			# no se cierra de manera adecuada
+			balance_file = open("balance_file.txt", "w", encoding="utf-8")
+			balance_file.write(str(monthly_expenses))
 
-		# SECCION QUE GUARDARA EL STATUS DE "monthly_expenses" EN UN ARCHIVO
+		except OSError as err:
+			print(f"ERROR :: Fallo durante el proceso de escritura: {err}")
 
-		break;
+			# EJERCICIO: Pedir confirmacion para salir de la ejecucion
+			# 	Estas seguro de salir ?  Se perdera los cambios. (s/n) 
+		
+		else: 
+			flow_ctrl = False
+
+		finally:
+			balance_file.close()
 
 	else:
 		print("OPCION NO VALIDA! Seleccion una opcion del menu.")
-
-
-print("Hasta la proxima 😎")
+		
+print("Hasta luego 🤓!")
